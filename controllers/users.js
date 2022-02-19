6bd55c370bdeb59d0ffd072cd47d2f687d7d6b25
LiuFFT <liufft98@gmail.com>
2022-02-19 17:09:18 +0800

    part13-exercises8

---
+++ controllers/users.js
@@ -0,0 +1,35 @@
+const router = require('express').Router()
+
+const { User } = require('../models')
+
+const userFinder = async (req, res, next) => {
+    req.user = await User.findOne({
+        where: {
+            username: req.params.username
+        }
+    })
+
+    next()
+}
+
+router.get('/', async (req, res) => {
+    const users = await User.findAll()
+    res.json(users)
+})
+
+router.post('/', async (req, res) => {
+    const user = await User.create(req.body)
+    res.json(user)
+})
+
+router.put('/:username', userFinder, async (req, res) => {
+    if (req.user) {
+        req.user.username = req.body.username
+        await req.user.save()
+        res.json(req.user)
+    } else {
+        res.status(404).end()
+    }
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const { PORT } = require('./util/config')
 const { connectToDatabase } = require('./util/db')
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users');
 const errorHandler = require('./middleware/errorhandler');
 
 
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(errorHandler)
 
