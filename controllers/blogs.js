a9c586ebaf2adcc5447045525808a168ee330682
LiuFFT <liufft98@gmail.com>
2022-02-19 16:16:09 +0800

    part13-exercises6

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -40,4 +40,16 @@ router.delete('/:id', blogFinder,  async (req, res) => {
     res.status(200).end()
 })
 
+
+// update likes
+router.put('/:id', blogFinder, async (req, res) => {
+    if (req.blog) {
+        req.blog.likes = req.body.likes
+        await req.blog.save()
+        res.json(req.blog)
+    } else {
+        res.status(404).end()
+    }
+})
+
 module.exports = router
