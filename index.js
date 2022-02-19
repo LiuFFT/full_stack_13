fa44856921570d103e3b3c6e98c3d826a07e40b1
LiuFFT <liufft98@gmail.com>
2022-02-19 16:46:06 +0800

    part13-exercises7

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express')
+require('express-async-errors')
 const app = express()
 
 const { PORT } = require('./util/config')
 const { connectToDatabase } = require('./util/db')
 
 const blogsRouter = require('./controllers/blogs')
+const errorHandler = require('./middleware/errorhandler');
+
 
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(errorHandler)
 
 const start = async () => {
   await connectToDatabase()
---
+++ middleware/errorhandler.js
@@ -0,0 +1,11 @@
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'SequelizeValidationError') {
+        return response.status(400).json({error})
+    }
+
+    next(error)
+}
+
+module.exports = errorHandler
