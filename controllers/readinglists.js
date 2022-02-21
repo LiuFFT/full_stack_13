a6f57507ba4b087cddfdbe740dbd2feb14125bfb
LiuFFT <liufft98@gmail.com>
2022-02-21 14:31:15 +0800

    part13-exercises22

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 
-const { Reading } = require("../models");
+const { Reading, User} = require("../models");
+const {tokenExtractor} = require("../middleware/errorhandler");
 
 router.post('/', async (req, res, next) => {
     const reading_list = await Reading.create(req.body)
@@ -10,6 +11,22 @@ router.post('/', async (req, res, next) => {
         blogId: reading_list.blogId
     }
     res.json(return_reading_list)
+})
+
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+    const user = await User.findByPk(req.decodedToken.id);
+    const reading_list = await Reading.findByPk(req.params.id)
+
+    if (user){
+        if(user.id === reading_list.user_id){
+            reading_list.read = true
+            await reading_list.save()
+            res.json(reading_list)
+        } else {
+            res.status(404).end()
+        }
+    }
 })
 
 module.exports = router
